62b6ab001cca7bda85d1782cf055bd82b07f61bb
Robson Locatelli <rlocatelli9@gmail.com>
2019-12-10 17:31:50 -0300

    Setting up FrontPunk App

---
+++ src/pages/Beer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Beer() {
+  return <h1>Beer</h1>;
+}
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Beer from './pages/Beer';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/beer" component={Beer} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
