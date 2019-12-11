e781ae89455be5d7671a009bc0fd6206996e38a9
Robson Locatelli <rlocatelli9@gmail.com>
2019-12-11 20:40:55 -0300

    Styling Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { FaBeer } from 'react-icons/fa';
-import { Container } from './styles';
+import { Container, Logo } from './styles';
+
+import logoPunk from '../../assets/logo.png';
 
 export default function Header() {
   return (
     <Container>
       <header>
         <nav>
-          <h1>WebPunk</h1>
+          <img src={logoPunk} alt="Logo Punk Beer" />
+
           <div>
             <FaBeer />
           </div>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -9,11 +9,17 @@ export const Container = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    width: 82vw;
     height: 64px;
     padding: 0 42px;
     color: #fff;
-    min-width: 980px;
+    min-width: 550px;
     font-size: 25px;
+  }
+
+  img {
+    width: 130px;
+    margin: auto 0px;
   }
 
   nav > div {
